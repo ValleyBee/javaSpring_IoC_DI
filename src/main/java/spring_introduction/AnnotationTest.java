8283fa822268f0f58e13e2d9c83b1121c0b6e022
VallenBee <csplus2010.vb@gmail.com>
2022-07-27 17:44:31 +0300

    add annotation

--- src/main/java/spring_introduction/AnnotationTest.java
+++ src/main/java/spring_introduction/AnnotationTest.java
@@ -1,14 +1,13 @@
 // Creating Objects by Spring annotation XML applicationContext_annotation.xml
 
 package spring_introduction;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class AnnotationTest {
     public static void main(String[] args) {
 
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_annotation.xml");
+   ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_annotation.xml");
 
 //        Cat myCat = context.getBean("catBean", Cat.class); // id in case we want to specify name.
 //        Cat myCat = context.getBean("cat", Cat.class); //  id for auto creating
--- src/main/java/spring_introduction/Mouse.java
+++ src/main/java/spring_introduction/Mouse.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+
+
 @Component
 @Scope("prototype")
 
